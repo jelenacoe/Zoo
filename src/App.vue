667eba9622158803ea4bf3a9b0e2c963b6e40202
ivan-vukosavljevic <jelena.coe@yahoo.com>
2021-05-18 20:04:54 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Zivotinje"/>
+    <animal-list msg="Lista Zivotinja"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import { AnimalList }from './components/AnimalList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AnimalList
   }
 }
 </script>
---
+++ src/components/AnimalList.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div>
+      <h3>{{ msg }}</h3>
+      <v-if="animal.date">
+        <v-else>Nepoznat</v-else>
+      <ul>
+        <li v-for-key="animal in animals">{{ animal.id+' '+ animal.ime + animal.date}}</li>
+      </ul>
+        <form @submit.prevent="addAnimal">
+        <input v-model="animal.ime" />
+        <input v-model="animal.age" />
+        <input v-model="animal.date"/>
+        <button @mouseover="submit">Add Animal</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AnimalList',
+    props: {
+        msg:String
+    },
+    data () {
+        return {
+            animals: [
+                {id:1, vrsta:0, ime: 'Konj', date: new Date(2018, 12, 24) },
+                {id:2, vrsta:0, ime: 'Svinja', date: new Date(2017, 1, 14) },
+                {id:3, vrsta: 1, ime: 'Lav', date: new Date(2012, 11, 20) }
+            ]
+        }
+    },
+    methods: {
+        addAnimal() {
+        this.animals.push({ name: this.animal.name, age: this.animal.age }); 
+        },
+        removeAnimal() {
+        
+        }
+    }
+}
+</script>
